61d97ac1f7edab3d5c2744762feb0076e4dbc9f0
hsw824 <ghdtjddn0824@naver.com>
2022-06-28 20:55:10 +0900

    Feat: Create movie search function

--- src/routes/Search/index.tsx
+++ src/routes/Search/index.tsx
@@ -1,15 +1,32 @@
 import { useState, ChangeEvent, MouseEvent } from 'react'
+import axios from 'axios'
+import { ProductProps } from 'types/ProductList'
+import { MovieList } from './MovieList'
+import Footer from 'components/Footer'
 
 import styles from './search.module.scss'
 
 const Search = () => {
   const [value, setValue] = useState('')
+  const [data, setData] = useState<ProductProps[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+  const API_KEY = process.env.REACT_APP_MOVIE_API_KEY
+
+  const MOVIE_BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=${value}&page={페이지번호(1)}`
+
+  const fetchData = async () => {
+    const response = await axios.get(`${MOVIE_BASE_URL}`)
+    setData(response.data.Search)
+  }
+
   const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.currentTarget.value)
   }
 
   const handleOnSubmit = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault()
+    fetchData()
+    setLoading(false)
     setValue('')
   }
   return (
@@ -20,6 +37,8 @@ const Search = () => {
           Search
         </button>
       </form>
+      {loading ? <h1>No search results found</h1> : <MovieList data={data} />}
+      <Footer />
     </div>
   )
 }
