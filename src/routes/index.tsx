630e5e0e18582e7edb678c3e4aec524860889407
hsw824 <ghdtjddn0824@naver.com>
2022-06-28 12:14:08 +0900

    Feat: Init project

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from 'react-router-dom'
+
+import styles from './routes.module.scss'
+
+const App = () => {
+  return <div className={styles.app}>하하</div>
+}
+
+export default App
